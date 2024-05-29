894d2767848490a3fc3e2346af9750d6210464ad
ShyWind <2513561399@qq.com>
2024-05-29 15:33:09 +0800

    完成所有登录注册功能

--- HQBlog-controller/src/main/java/com/shywind/hqblog/Application.java
+++ HQBlog-controller/src/main/java/com/shywind/hqblog/Application.java
@@ -1,21 +1,18 @@
 package com.shywind.hqblog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动类
  *
  */
+//@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @SpringBootApplication
+@MapperScan("com.shywind.hqblog.mapper")
 public class Application {
-    /**
-     * @description DONE
-     * @params args
-     * @return void
-     * @author ShyWind
-     * @date 2024/5/13 20:43
-     */
     public static void main( String[] args )
     {
         SpringApplication.run(Application.class, args);
--- HQBlog-controller/src/main/java/com/shywind/hqblog/Controller/LoginController.java
+++ HQBlog-controller/src/main/java/com/shywind/hqblog/Controller/LoginController.java
@@ -1,9 +1,14 @@
 package com.shywind.hqblog.Controller;
 
 import com.shywind.hqblog.Result.Result;
+import com.shywind.hqblog.VO.LoginCodeInfoVO;
+import com.shywind.hqblog.VO.LoginPasswordInfoVO;
+import com.shywind.hqblog.VO.RegisterInfoVO;
 import com.shywind.hqblog.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,7 +23,59 @@ public class LoginController {
      * @date 2024/5/13 20:52
      */
     @GetMapping("/login/code")
-    public Result email(String email){
-        return loginService.sendEmailCode(email);
+    public Result loginByCode(String email){
+        return loginService.loginByCode(email);
+    }
+
+
+    /**
+     * @description TODO 
+     * @params loginInfo
+     * @return com.shywind.hqblog.Result.Result
+     * @author ShyWind
+     * @date 2024/5/29 11:35
+     */
+    @PostMapping("/login/password")
+    public Result loginByPassword(@RequestBody LoginPasswordInfoVO loginPasswordInfo){return loginService.loginByPassword(loginPasswordInfo);}
+
+    /**
+     * @description TODO
+     * @params email
+     * @return com.shywind.hqblog.Result.Result
+     * @author ShyWind
+     * @date 2024/5/29 10:45
+     */
+    @GetMapping("/register/code")
+    public Result sendRegisterEmail(String email){ return loginService.sendRegisterEmailCode(email);}
+
+    
+    /**
+     * @description TODO 
+     * @params loginPasswordInfo       
+     * @return com.shywind.hqblog.Result.Result
+     * @author ShyWind
+     * @date 2024/5/29 11:49
+     */
+    @PostMapping("/login/code/submit")
+    public Result codeLoginSubmit(@RequestBody LoginCodeInfoVO loginCodeInfo) {
+        return loginService.codeLoginSubmit(loginCodeInfo);
+    }
+
+
+    /**
+     * @description TODO
+     * @params registerInfo
+     * @return com.shywind.hqblog.Result.Result
+     * @author ShyWind
+     * @date 2024/5/21 14:01
+     */
+    @PostMapping("/register/submit")
+    public Result registerSubmit(@RequestBody RegisterInfoVO registerInfo) {
+        return loginService.registerSubmit(registerInfo);
+    }
+
+    @GetMapping("/test")
+    public String test(String a) {
+        return a;
     }
 }
